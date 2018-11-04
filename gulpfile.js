7668cadcb861c30d1b23aa9b0d6055491ea6c8ba
paulyarin <pavyarin@yandex.ru>
2018-11-04 19:44:56 +0700

    Написал основные задачи для работы

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var server = require("browser-sync").create();
+var autoprefixer = require("autoprefixer");
+
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(less())
+    .pipe(gulp.dest("source/css"));
+});
+
+gulp.task("server", function () {
+  server.init ({
+    server: "source/"
+  })
+  
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+})
+
+gulp.task("debug", gulp.series("css", "server"));
